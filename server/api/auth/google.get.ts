71d89290434ea34afc2677926d8c72f8f53b1caa
Bagas Yadher Bima Nugraha Ainur Rofiq Hidayat <bagas.ybnarh@gmail.com>
2025-02-08 20:08:28 +0700

    Trying to log env

--- server/api/auth/google.get.ts
+++ server/api/auth/google.get.ts
@@ -1,6 +1,12 @@
-export default defineOAuthGoogleEventHandler({
-  async onSuccess(event, { user }) {
-    await setUserSession(event, { user })
-    return sendRedirect(event, '/todos')
-  }
+export default defineEventHandler(async (event) => {
+  console.log(process.env.NUXT_OAUTH_GOOGLE_CLIENT_ID)
+  console.log(process.env.NUXT_OAUTH_GOOGLE_CLIENT_SECRET)
+  console.log(process.env.NUXT_OAUTH_GOOGLE_REDIRECT_URI)
+
+  return defineOAuthGoogleEventHandler({
+    async onSuccess(event, { user }) {
+      await setUserSession(event, { user })
+      return sendRedirect(event, '/todos')
+    }
+  })(event)
 })
