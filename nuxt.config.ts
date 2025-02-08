411c609c47be1f3c800e3e57a799af2746a0f8f8
Bagas Yadher Bima Nugraha Ainur Rofiq Hidayat <bagas.ybnarh@gmail.com>
2025-02-08 21:21:21 +0700

    Changec config again

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,33 +1,36 @@
 export default defineNuxtConfig({
   modules: [
-    '@nuxthub/core',
-    '@nuxt/ui',
-    'nuxt-auth-utils',
-    '@nuxt/eslint',
-    '@pinia/nuxt',
-    '@pinia/colada-nuxt'
+    "@nuxthub/core",
+    "@nuxt/ui",
+    "nuxt-auth-utils",
+    "@nuxt/eslint",
+    "@pinia/nuxt",
+    "@pinia/colada-nuxt",
   ],
   devtools: {
-    enabled: true
+    enabled: true,
   },
   future: { compatibilityVersion: 4 },
-  compatibilityDate: '2024-07-30',
+  compatibilityDate: "2024-07-30",
   hub: {
-    database: true
+    database: true,
   },
   // Development config
   eslint: {
     config: {
       stylistic: {
-        quotes: 'single',
-        commaDangle: 'never'
-      }
-    }
+        quotes: "single",
+        commaDangle: "never",
+      },
+    },
   },
-  auth: {
-    google: {
-      clientId: process.env.NUXT_OAUTH_GOOGLE_CLIENT_ID,
-      clientSecret: process.env.NUXT_OAUTH_GOOGLE_CLIENT_SECRET
-    }
-  }
-})
+  runtimeConfig: {
+    oauth: {
+      google: {
+        clientId: process.env.NUXT_OAUTH_GOOGLE_CLIENT_ID,
+        clientSecret: process.env.NUXT_OAUTH_GOOGLE_CLIENT_SECRET,
+        redirectURL: process.env.NUXT_OAUTH_GOOGLE_REDIRECT_URI,
+      },
+    },
+  },
+});
